db248832b19e282404452f38829b50e8a2cdf63c
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 17:41:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+People = ["Edsger", "Ada", "Charles", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+return "Hello, my name is #{name}"
+end
+
+def batch_badge_maker_creator(array)
+    nuarray= []
+    counter = 1
+    array.each do |name|
+    nuarray.push("Hello, #{name} you'll be assign to room #{counter}!")
+    counter += 1
+  end 
+  return nuarray 
+end 
+#assign_rooms(people)
+
+   def printer(array)
+     batch_badge_creator(array).each do |id| 
+       puts id 
+     end 
+     
+    
