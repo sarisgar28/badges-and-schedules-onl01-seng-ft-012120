b52f3ce5406a8690d6e7ae7ee71eb140586bcd50
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-27 17:13:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 batch_badge_creator = ("Edsger, Ada, Charles, Grace, Linus, Matz ")
 => "Hello, my name is #{name}."
-def assign_rooms.each_with_index = |"i.1|, 2, 3, 4, 5, 6, 7")
+def assign_rooms.each_with_index = |"i.1|,)
   
